932eae4a09ebeb06192b9c1022ba67a0cccf52f5
Unknown <bruno@leaoteixeira.com.br>
2018-11-03 17:09:38 -0300

    Player 2 e Morte

--- Grupo 2 Game/Assets/Scripts/Player.cs
+++ Grupo 2 Game/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public bool player1;
     public float Velocity = 5f;
     public float JumpVelocity = 5f;
     public int JumpCountMax = 2;
@@ -19,32 +20,62 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Vector2 totalVel = Vector2.zero;
-        //ir para a esquerda
-		if(Input.GetKey(KeyCode.A))
-        {
-            totalVel += Vector2.left * Velocity;
-        }
-        //ir para a direita
-        else if (Input.GetKey(KeyCode.D))
-        {
-            totalVel += Vector2.right * Velocity;
-        }
-        //parar
-        else 
-        {
-            Vector2 vela = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
-            GetComponent<Rigidbody2D>().velocity = vela;
-        }
+        if(player1){
+            Vector2 totalVel = Vector2.zero;
+            //ir para a esquerda
+            if(Input.GetKey(KeyCode.A))
+            {
+                totalVel += Vector2.left * Velocity;
+            }
+            //ir para a direita
+            else if (Input.GetKey(KeyCode.D))
+            {
+                totalVel += Vector2.right * Velocity;
+            }
+            //parar
+            else 
+            {
+                Vector2 vela = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+                GetComponent<Rigidbody2D>().velocity = vela;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
-        {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpVelocity, ForceMode2D.Impulse);
-            jumpCount--;
-        }
+            if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpVelocity, ForceMode2D.Impulse);
+                jumpCount--;
+            }
 
-        Vector2 vel = new Vector2(totalVel.x, GetComponent<Rigidbody2D>().velocity.y);
-        GetComponent<Rigidbody2D>().velocity = vel;
+            Vector2 vel = new Vector2(totalVel.x, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = vel;
+        }
+        else{
+            Vector2 totalVel = Vector2.zero;
+            //ir para a esquerda
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                totalVel += Vector2.left * Velocity;
+            }
+            //ir para a direita
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                totalVel += Vector2.right * Velocity;
+            }
+            //parar
+            else 
+            {
+                Vector2 vela = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+                GetComponent<Rigidbody2D>().velocity = vela;
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && jumpCount > 0)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * JumpVelocity, ForceMode2D.Impulse);
+                jumpCount--;
+            }
+
+            Vector2 vel = new Vector2(totalVel.x, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = vel;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -52,6 +83,12 @@ public class Player : MonoBehaviour {
         if(collision.gameObject.CompareTag("platform"))
         {
             jumpCount = JumpCountMax;
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collider2D){
+        if(collider2D.gameObject.CompareTag("chao"))
+        {
+            Destroy(gameObject);
         }
     }
 }
