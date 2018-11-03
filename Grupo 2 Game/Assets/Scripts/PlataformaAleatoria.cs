f68cbb8b12e872aa2cc8cbac1bb1bf8e6773fef6
Michelle Santiago Beadle <michelle.santiago10@gmail.com>
2018-11-03 16:37:03 -0300

    ultima versao da plataforma

--- Grupo 2 Game/Assets/Scripts/PlataformaAleatoria.cs
+++ Grupo 2 Game/Assets/Scripts/PlataformaAleatoria.cs
@@ -8,15 +8,31 @@ public class PlataformaAleatoria : MonoBehaviour {
 
     private float _screenWidth;
 
+    private List<GameObject> _presentPlataforms = new List<GameObject>();
+
 	// Use this for initialization
 	void Start () {
-        _screenWidth = Screen.width;
-
+        
+        _screenWidth = Camera.main.rect.width;
+        StartCoroutine(MovingPlataform());
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (_presentPlataforms.Count > 0)
+        {
+            foreach (GameObject plat in _presentPlataforms)
+            {
+                if (plat.transform.position.y < -1*Camera.main.rect.height - 150)
+                {
+                    _presentPlataforms.Remove(plat);
+                    Destroy(plat);
+                }
+
+            }
+        }
+        
 	}
 
 
@@ -24,7 +40,25 @@ public class PlataformaAleatoria : MonoBehaviour {
     {
         float plataformWidth = Plataform.GetComponent<RectTransform>().rect.width;
 
-        Vector3 spawnPos = transform.TransformPoint(Random.Range(-1 * _screenWidth/2,_screenWidth/2), Screen.height + 10, 0);
-        Instantiate(Plataform, spawnPos, Plataform.transform.rotation);
+        Vector3 spawnPos = transform.TransformPoint(Random.Range(Camera.main.rect.xMin, _screenWidth ), Camera.main.rect.height + 10, 0);
+        GameObject newPlataform = Instantiate(Plataform, spawnPos, Plataform.transform.rotation);
+
+        _presentPlataforms.Add(newPlataform);
+        movePlataformDowards(newPlataform);
+    }
+
+    void movePlataformDowards(GameObject plataform)
+    {
+        plataform.transform.position = Vector3.Slerp(plataform.transform.position,new Vector3(plataform.transform.position.x, plataform.transform.position.y - 10, 0), 0);
+    }
+
+    IEnumerator MovingPlataform()
+    {
+        while (true)
+        {
+            randomizePlataformPlace();
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
