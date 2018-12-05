3164e4df7c9d14d4390f034834654e325074890d
Felipe Pessoa <felipessoaf@hotmail.com>
2018-12-05 07:16:27 -0200

    tutorial

---
+++ Grupo 2 Game/Assets/Scripts/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject Tutorial;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ActivateTutorial(false);
+        }
+    }
+
+    public void changeScene(string scene)
+    {
+		SceneManager.LoadScene("SampleScene");
+	}
+
+    public void ActivateTutorial(bool active)
+    {
+        Tutorial.SetActive(active);
+    }
+}
