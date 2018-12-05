53b8ff896b916af06e42e7b2e303ac5724999003
Gabriel Oliveira <gabriel90939@gmail.com>
2018-12-04 23:33:07 -0200

    Adicionando transição da tela inicial pra tela de jogo

---
+++ Grupo 2 Game/Assets/Scripts/StartButtonAction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonAction : MonoBehaviour {
+	public void changeScene(string scene) {
+		SceneManager.LoadScene("SampleScene");
+	}
+}
